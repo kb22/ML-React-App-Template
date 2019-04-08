147d79a26236855ae5d423e9bd6848111df31e91
Bhanot, Karan <k.bhanot@cvent.com>
2019-04-08 15:45:04 +0530

    Add basic heading and form fields

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <div>
+          <h1 className="title">This is a title</h1>
+        </div>
+        <div className="content">
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+          </Form>
+        </div>
+      </Container>
     );
   }
 }
 
 export default App;
+
+      // <div className="App">
+      //   <div className="title">
+      //     <h1>ML App</h1>
+      //   </div>
+      //   <Form>
+      //     <Form.Row>
+      //       <Form.Group as={Col} controlId="formGridEmail">
+      //         <Form.Label>Email</Form.Label>
+      //         <Form.Control type="email" placeholder="Enter email" />
+      //       </Form.Group>
+
+      //       <Form.Group as={Col} controlId="formGridPassword">
+      //         <Form.Label>Password</Form.Label>
+      //         <Form.Control type="password" placeholder="Password" />
+      //       </Form.Group>
+      //     </Form.Row>
+
+      //     <Form.Group controlId="formGridAddress1">
+      //       <Form.Label>Address</Form.Label>
+      //       <Form.Control placeholder="1234 Main St" />
+      //     </Form.Group>
+
+      //     <Form.Group controlId="formGridAddress2">
+      //       <Form.Label>Address 2</Form.Label>
+      //       <Form.Control placeholder="Apartment, studio, or floor" />
+      //     </Form.Group>
+
+      //     <Form.Row>
+      //       <Form.Group as={Col} controlId="formGridCity">
+      //         <Form.Label>City</Form.Label>
+      //         <Form.Control />
+      //       </Form.Group>
+
+      //       <Form.Group as={Col} controlId="formGridState">
+      //         <Form.Label>State</Form.Label>
+      //         <Form.Control as="select">
+      //           <option>Choose...</option>
+      //           <option>...</option>
+      //         </Form.Control>
+      //       </Form.Group>
+
+      //       <Form.Group as={Col} controlId="formGridZip">
+      //         <Form.Label>Zip</Form.Label>
+      //         <Form.Control />
+      //       </Form.Group>
+      //     </Form.Row>
+
+      //     <Form.Group id="formGridCheckbox">
+      //       <Form.Check type="checkbox" label="Check me out" />
+      //     </Form.Group>
+
+      //     <Button variant="primary" type="submit">
+      //       Submit
+      //     </Button>
+      //   </Form>;
+      // </div>
